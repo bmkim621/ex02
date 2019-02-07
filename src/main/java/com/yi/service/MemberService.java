6e11b15edc22e955376c8b0cc1e14277ea75e3c5
bmkim621 <moonstruck0614@gmail.com>
2019-02-07 15:06:48 +0900

    MemberService

--- src/main/java/com/yi/service/MemberService.java
+++ src/main/java/com/yi/service/MemberService.java
@@ -1,5 +1,21 @@
 package com.yi.service;
 
-public interface MemberService {
+import java.util.List;
 
+import com.yi.domain.MemberVO;
+
+public interface MemberService {
+	
+	public String getTime();
+	
+	public void insertMember(MemberVO vo);
+	
+	public MemberVO readMember(String userid);
+
+	public List<MemberVO> selectAll();
+	
+	//아이디가 XX인 이름, 비밀번호, 이메일 변경
+	public void updateMember(MemberVO vo);
+	
+	public void deleteMember(String userid);
 }
